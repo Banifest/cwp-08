811e311a2d8490520edb9f4e22a452c363afc7df
banifest <banifest@gmail.com>
2017-10-17 00:25:07 +0300

    complete first task (write rand numbers in file)

--- worker.js
+++ worker.js
@@ -1,18 +1,18 @@
-fs =
+const fs = require("fs");
 
 let jsonFile = process.argv[2];
 let X = process.argv[3];
 
-writer =  fs.createWriteStream(jsonFile, {start: -1});
-
-
-
-
-while (true)
+fs.writeFile(jsonFile, "[]", ()=>
 {
-    setTimeout(()=>
+    setInterval(()=>
                {
-
-                   fs.appendFile(jsonFile, Number(Math.random()*100))
-               }, X)
-}
+                   console.log(1);
+                   fs.stat(jsonFile, (err, status)=>
+                   {
+                       let writer =  fs.createWriteStream(jsonFile, {flags: 'r+', start: (status.size - 1)});
+                       let separator = status.size !== 2? ',': '';
+                       writer.write(`${separator} ${Math.trunc(Math.random() * 100)}]`, ()=> {});
+                   });
+               }, X * 1000);
+});
